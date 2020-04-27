9cb6c615a886bb429b5b05807e39065891dfcd96
Harrison Price <hprice518@gmail.com>
2020-04-27 02:33:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key_for_min_value.map do |item, price|
-    value
+  # key_for_min_value.map do |item, price|
+  #   value
   end
 end
